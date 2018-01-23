b6ba793dbb22917d98b38284105f05d737058b32
Orkhan Farajov <orkhankhf@code.edu.az>
2018-01-23 18:39:04 +0400

    CommonMethods class'inda placeholderle bagli ciddi deyisiklikler olundu

---
+++ StudentEvaluationSystem/StudentEvaluationSystem/CommonMethods.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace StudentEvaluationSystem
+{
+	class CommonMethods
+	{
+		//bos textboxlarin icine placeholder elave edir
+		public void PlaceHolderMaker(TextBox _textbox, string _placeholder)
+		{
+			if (_textbox.Text == "")
+			{
+				_textbox.Text = _placeholder;
+				_textbox.ForeColor = Color.Gray;
+			}
+		}
+        public void PlaceHolderEdit(TextBox _textbox, string _placeholder)
+        {
+            if (_textbox.Text == _placeholder)
+            {
+                _textbox.Text = "";
+                _textbox.ForeColor = Color.Black;
+            }
+        }
+    }
+}
---
+++ StudentEvaluationSystem/StudentEvaluationSystem/TeacherLoginPage.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StudentEvaluationSystem
+{
+	public partial class TeacherLoginPage : Form
+	{
+		CommonMethods CommonMethods = new CommonMethods();
+		string placeHolderString;
+		public TeacherLoginPage()
+		{
+			InitializeComponent();
+		}
+
+		private void Window_Close_Btn_Click(object sender, EventArgs e)
+		{
+			Application.Exit();
+		}
+
+		private void TeacherLoginPage_Load(object sender, EventArgs e)
+		{
+			CommonMethods.PlaceHolderMaker(Teacher_Login_Email, "Enter e-mail");
+			CommonMethods.PlaceHolderMaker(Teacher_Login_Password, "Enter password");
+		}
+
+		private void TextBoxChanged(object sender, KeyPressEventArgs e)
+		{
+			TextBox currentTextBox = sender as TextBox;
+            switch(currentTextBox.Name)
+			{
+				case "Teacher_Login_Email" :
+					placeHolderString = "Enter e-mail";
+					break;
+				case "Teacher_Login_Password":
+					placeHolderString = "Enter password";
+					break;
+			}
+			CommonMethods.PlaceHolderEdit(currentTextBox, placeHolderString);
+		}
+    }
+}
